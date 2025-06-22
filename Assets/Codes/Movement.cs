9045447ab3674b4b049d3e7213fc40f087d6c4ba
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2025-06-22 17:25:26 +0300

    Collider Added

--- Assets/Codes/Movement.cs
+++ Assets/Codes/Movement.cs
@@ -1,8 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float speed = 0.02f;
+     [SerializeField] float rotationSpeed = 0.01f;
+     [SerializeField] float speed = 0.01f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,7 +16,9 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, speed);
-        transform.Translate(0, speed, 0);
+        float horizontalInput = Input.GetAxis("Horizontal") * rotationSpeed;
+        float verticalInput = Input.GetAxis("Vertical") * speed;
+        transform.Rotate(0, 0, horizontalInput * Time.deltaTime);
+        transform.Translate(0, verticalInput * Time.deltaTime, 0);
     }
 }
