de777631c9ef6fba86747e3fe5ec65d411855c60
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2025-06-25 15:15:03 +0300

    Added new features

--- Assets/Codes/Collision.cs
+++ Assets/Codes/Collision.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    private bool isTouched = false; 
+    [SerializeField] float destroyDelay = 0.5f; // Delay before destroying the package
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1); 
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1); 
+    private bool isTouched = false;
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -11,15 +19,18 @@ public class Collision : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Package")
+        if (collision.tag == "Package" && !isTouched)
         {
             Debug.Log("Player got the Package");
             isTouched = true;
+            spriteRenderer.color = hasPackageColor; // Change color to indicate package is picked up
+            Destroy(collision.gameObject, destroyDelay); // Destroy the package after 0.5 seconds
         }
 
         if (collision.tag == "Customer" && isTouched)
         {
             Debug.Log("Customer Touched");
+            spriteRenderer.color = noPackageColor; // Change color back to indicate package is delivered
             isTouched = false; // Reset after delivering the package
         }
     }
--- Assets/Codes/Movement.cs
+++ Assets/Codes/Movement.cs
@@ -1,16 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-     [SerializeField] float rotationSpeed = 0.01f;
-     [SerializeField] float speed = 0.01f;
+    [SerializeField] float rotationSpeed = 0.01f;
+    [SerializeField] float speed = 0.01f;
+    [SerializeField] float boostSpeed = 0.02f; // Speed when boosting
+    [SerializeField] float boostDuration = 2f; // Duration of the boost effect
+    [SerializeField] float slowSpeed = 0.005f; // Speed when slowing down
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,4 +25,20 @@ public class Movement : MonoBehaviour
         transform.Rotate(0, 0, -horizontalInput * Time.deltaTime);
         transform.Translate(0, verticalInput * Time.deltaTime, 0);
     }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        speed = slowSpeed; // Slow down on collision
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "BoostSpeed")
+        {
+            Debug.Log("Boost Speed Triggered");
+            speed = boostSpeed; // Boost speed when entering the trigger
+        }
+    }
+
 }
